8002c10e2ba32351fa49d804247006b7dc6a8b89
Gomez Maximiliano <maxil5gomez@gmail.com>
2024-01-23 18:16:16 -0300

    Se creo estructura y algunos estilos de componente de contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,44 +2,74 @@ const Contact = () => {
   return (
     <div className="contact__container">
       <div className="contact__info--container">
-        <h2>Datos Personales:</h2>
-        <h3>NOMBRE Y APELLIDO:</h3>
+        <h2 className="contact__title">DATOS PERSONALES:</h2>
+        <h4>NOMBRE Y APELLIDO:</h4>
         <p>Gómez Maximiliano Ezequiel.</p>
-        <h3>DNI:</h3>
+        <h4>DNI:</h4>
         <p>38.502.162</p>
-        <h3>LUGAR Y FECHA DE NACIMIENTO</h3>
+        <h4>LUGAR Y FECHA DE NACIMIENTO:</h4>
         <p>Córdoba, Argentina - 25 de Octubre de 1994</p>
-        <h3>CORREO ELECTRÓNICO</h3>
+        <h4>CORREO ELECTRÓNICO:</h4>
         <p>mmsg.2594@gmail.com</p>
-        <h3>NUMERO DE TELÉFONO</h3>
+        <h4>NUMERO DE TELÉFONO:</h4>
         <p>351-3740384</p>
-        <h3>DIRECCION</h3>
+        <h4>DIRECCION:</h4>
         <p>Barrio Colinas del Cerro, Córdoba </p>
-        <h3>ESTADO CIVIL</h3>
+        <h4>ESTADO CIVIL:</h4>
         <p>Soltero</p>
       </div>
       <div className="contact__form--container">
-        <form name="contact">
-          <h2>Trabaja Conmigo!</h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
-            asperiores? Doloremque quia, amet perspiciatis ex ipsam dolorem
-            similique quasi dolores, sit eum, facilis voluptatum alias at
-            obcaecati quaerat perferendis unde.
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="name">Nombre</label>
-            <input type="text" id="name" name="name" />
+        <h2 className="contact__title">¡Trabaja Conmigo!</h2>
+        <p>
+          Si deseas comunicarte conmigo, completa el siguiente formulario y
+          responderé a la brevedad.
+        </p>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Nombre y apellido:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
           </div>
-          <div>
-            <label htmlFor="email">Correo electrónico</label>
-            <input type="email" id="email" name="email" />
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Correo electrónico
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
           </div>
-          <div>
-            <label htmlFor="message">Mensaje</label>
-            <textarea name="message" id="message" />
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail2" className="form-label">
+              Motivo:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail2"
+              aria-describedby="emailHelp"
+            />
           </div>
-          <button type="submit">Enviar</button>
+
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1">Mensaje:</label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     </div>
